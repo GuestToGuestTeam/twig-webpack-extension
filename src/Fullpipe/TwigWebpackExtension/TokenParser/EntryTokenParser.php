9d139e32e64c400a5b7e2239674d9d8d69d5a09b
Théotime Loiseau <theotime@guesttoguest.com>
2019-08-27 15:30:46 +0200

    Add support for webpack-dev-server manifests

--- src/Fullpipe/TwigWebpackExtension/TokenParser/EntryTokenParser.php
+++ src/Fullpipe/TwigWebpackExtension/TokenParser/EntryTokenParser.php
@@ -17,13 +17,28 @@ abstract class EntryTokenParser extends \Twig_TokenParser
         $this->publicPath = $publicPath;
     }
 
+    private function does_url_exist($url) {
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_NOBODY, true);
+        curl_exec($ch);
+        $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+        if ($code == 200) {
+            $status = true;
+        } else {
+            $status = false;
+        }
+        curl_close($ch);
+        return $status;
+    }
+
     public function parse(\Twig_Token $token)
     {
         $stream = $this->parser->getStream();
         $entryName = $stream->expect(\Twig_Token::STRING_TYPE)->getValue();
         $stream->expect(\Twig_Token::BLOCK_END_TYPE);
 
-        if (!file_exists($this->manifestFile)) {
+        if (!file_exists($this->manifestFile) && !$this->does_url_exist($this->manifestFile)) {
             throw new \Twig_Error_Loader(
                 'Webpack manifest file not exists.',
                 $token->getLine(),
